f8afa145547f094d804050003f63c86732b1f56d
bkismo <botond.kismoni@cloudera.com>
2021-08-28 14:58:12 +0200

    Fixed CheckStyle and FindBugs issues

--- cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/commonapi/CommonApi.java
+++ cruise-control/src/main/java/com/linkedin/kafka/cruisecontrol/commonapi/CommonApi.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2017 LinkedIn Corp. Licensed under the BSD 2-Clause License (the "License"). See License in the project root for license information.
+ */
 package com.linkedin.kafka.cruisecontrol.common_api;
 
 import com.linkedin.cruisecontrol.servlet.EndPoint;
